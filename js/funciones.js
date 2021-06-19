e46b37c6b3907a580473020ea6ff0eb5e2369c36
Ezequiel Ramirez <ezequielram@gmail.com>
2021-06-18 22:11:28 -0300

    creacion de section detalle con js

---
+++ js/funciones.js
@@ -0,0 +1,31 @@
+$("#btnCurso1").click(function (e) { 
+    e.preventDefault();
+   $(".detail-1").toggle();    
+});
+$("#btnCurso2").click(function (e) { 
+    e.preventDefault();
+   $(".detail-2").toggle();    
+});
+$("#btnCurso3").click(function (e) { 
+    e.preventDefault();
+   $(".detail-3").toggle();    
+});
+$("#btnCurso4").click(function (e) { 
+    e.preventDefault();
+   $(".detail-4").toggle();    
+});
+$("#btnCurso5").click(function (e) { 
+    e.preventDefault();
+   $(".detail-5").toggle();    
+});
+$("#btnCurso6").click(function (e) { 
+    e.preventDefault();
+   $(".detail-6").toggle();    
+});
+
+$(".btnCurso").click(function (e) { 
+    e.preventDefault();
+    $(".detail-1, .detail-2, .detail-3, .detail-4, .detail-5, .detail-6").css("display", "none");
+                
+    
+});
