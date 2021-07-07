dfbcc9499195fac789bc61df16e9a58ec35943ec
Ezequiel Ramirez <ezequielram@gmail.com>
2021-07-07 17:43:09 -0300

    ajuste 13

--- js/funciones.js
+++ js/funciones.js
@@ -42,7 +42,7 @@ var caja_trasera_login = document.querySelector(".caja__trasera-login");
 var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
 function anchoPagina(){
-    if(window.innerWidth > 920){
+    if(window.innerWidth > 480){
         caja_trasera_login.style.display = "block";
         caja_trasera_register.style.display = "block";
     }else{
@@ -57,7 +57,7 @@ function anchoPagina(){
 //anchoPagina();
 
 function iniciarSesion(){
-    if(window.innerWidth > 920){
+    if(window.innerWidth > 480){
         formulario_register.style.display ="none";
         contenedor_login_register.style.left = "10px";
         formulario_login.style.display = "block";
@@ -73,7 +73,7 @@ function iniciarSesion(){
 };
 
 function register(){
-    if(window.innerWidth > 920){
+    if(window.innerWidth > 480){
         formulario_register.style.display ="block";
         contenedor_login_register.style.left = "410px";
         formulario_login.style.display = "none";
